6aebe493f75dba9f635126a5c3ace41a50ea51be
TomaszT <trzpistomasz8@gmail.com>
2020-08-17 21:49:51 +0200

    init

---
+++ src/main/java/org/t3pis/bmg/BmgApplication.java
@@ -0,0 +1,13 @@
+package org.t3pis.bmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/t3pis/bmg/BmgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.t3pis.bmg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
